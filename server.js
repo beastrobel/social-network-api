e637d91cd17a4b7f65e46f399182b1f651666c89
beastrobel <beatrizstrobel@proton.me>
2024-01-01 22:53:54 -0500

    Connected server to API routes

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const express = require('express');
-// Run npm install mongodb and require mongodb and MongoClient class
 const { MongoClient } = require('mongodb');
-
+const routes = require('./routes');
 const app = express();
 const port = 3001;
 
-// Connection string to local instance of MongoDB
 const connectionStringURI = `mongodb://127.0.0.1:27017`;
 
-// Initialize a new instance of MongoClient
 const client = new MongoClient(connectionStringURI);
 
-// Declare a variable to hold the connection
 let db;
 
-// Create variable to hold our database name
 const dbName = 'socialnetworkDB';
 
-// Use connect method to connect to the mongo server
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
 client.connect()
   .then(() => {
     console.log('Connected successfully to MongoDB');
-    // Use client.db() constructor to add new db instance
     db = client.db(dbName);
 
-    // start up express server
     app.listen(port, () => {
       console.log(`Social network listening at http://localhost:${port}`);
     });
@@ -33,27 +29,4 @@ client.connect()
     console.error('Mongo connection error: ', err.message);
   });
 
-// Built in Express function that parses incoming requests to JSON
-app.use(express.json());
 
-app.post('/create', (req, res) => {
-  // Use db connection to add a document
-  db.collection('users').insertOne(
-    { username: req.body.username, email: req.body.email }
-  )
-    .then(results => res.json(results))
-    .catch(err => {
-      if (err) throw err;
-    });
-});
-
-app.get('/read', (req, res) => {
-  // Use db connection to find all documents in collection
-  db.collection('users')
-    .find()
-    .toArray()
-    .then(results => res.json(results))
-    .catch(err => {
-      if (err) throw err;
-    });
-});
